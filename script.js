0f521d2a59086d958e7204f2eb88abafba3127f1
Tanuj <khiladitanuj@gmail.com>
2023-11-12 18:35:52 +0530

    section-2

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true,
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector(".main"),
+//   smooth: true,
+// });
 
-var tl = gsap.timeline();
+// var tl = gsap.timeline();
 
-tl.to(".main", {});
+// tl.to(".main", {});
 
-tl.to(".page1", {
-  y: "100vh",
-  scale: "0.5",
-  duration: 0,
-});
-tl.to(".page1", {
-  y: "0vh",
-  delay: 0.5,
-  duration: 1,
-});
-tl.to(".page1", {
-  rotate: -360,
-  scale: 1,
-  delay: 0.7,
-  duration: 1,
-});
+// tl.to(".page1", {
+//   y: "100vh",
+//   scale: "0.5",
+//   duration: 0,
+// });
+// tl.to(".page1", {
+//   y: "0vh",
+//   delay: 0.5,
+//   duration: 1,
+// });
+// tl.to(".page1", {
+//   rotate: -360,
+//   scale: 1,
+//   delay: 0.7,
+//   duration: 1,
+// });
